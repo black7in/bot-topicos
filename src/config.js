3405c8aeb1a01b5ba7fe387d362301f14b36ee34
Black7in <elperroblack158@gmail.com>
2022-09-05 13:30:59 -0400

    eliminar dialogflow

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 module.exports = {
     // fb credentials
     VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN || '',
-    PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN || '',
+    PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN || ''
 
     // google credentials
-    
+
 
 
 }
