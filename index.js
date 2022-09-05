5f8f0ae49633525f894c301683ea831d480789ee
Black7in <elperroblack158@gmail.com>
2022-09-05 10:37:08 -0400

    fix: port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
 function main(){
-    app.listen((5000), () => console.log('El servidor esta escchando!'));
+    app.listen((process.env.PORT || 5000), () => console.log('El servidor esta escchando!'));
 }
 main();
