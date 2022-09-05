d085d758fa0290b29b55606288147c1b83901170
Black7in <elperroblack158@gmail.com>
2022-09-05 12:57:55 -0400

    sin credenciales de goole

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan")
 const bodyParser = require("body-parser");
 
-const test = require('./google-credentials.json')
+//const test = require('./google-credentials.json')
 
 console.log(test);
 
