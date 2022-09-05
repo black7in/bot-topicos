74bc84a7457ed92e802c99de761d89182b7b49d1
Black7in <elperroblack158@gmail.com>
2022-09-05 11:06:22 -0400

    enlanzar con pagina de facebook

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    // fb credentials
+    VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN || '',
+    PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN || ''
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('./controllers/home')
+const homeController = require('./controllers/home');
+const chatBotCOntroller = require('./controllers/chatBot');
 
 router.get("/", homeController.getHome);
+router.get("/webhook", chatBotCOntroller.getWebhook);
+router.post("/webhook", chatBotCOntroller.postWebhook);
 
 module.exports = router;
